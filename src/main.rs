82a45a360e6b5be1d2a9ddb0a730e03316caca2d
Juraj Michalek <juraj.michalek@gmail.com>
2023-03-05 09:54:28 +0100

    render input file

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@ use std::io::Read;
 use textwrap::{wrap};
 use image::{ImageBuffer, Rgb};
 
-const MAX_WIDTH: u32 = 900;
+const MAX_WIDTH: u32 = 960;
+const MAX_HEIGHT: u32 = 540;
+const CALENDAR_X_OFFSET: i32 = 200;
 
 fn main() {
     // Load text from file
@@ -23,26 +25,33 @@ fn main() {
     let height = line_height * lines.len() as u32 + padding * 2;
 
     // Create image buffer
-    let mut img = ImageBuffer::<Rgb<u8>, _>::new(MAX_WIDTH, height);
+    let mut img = ImageBuffer::<Rgb<u8>, _>::new(MAX_WIDTH, MAX_HEIGHT);
     img.fill(255);
 
     // Draw text onto image buffer
     let font = rusttype::Font::try_from_bytes(include_bytes!("../DejaVuSans.ttf")).unwrap();
-    let scale = rusttype::Scale::uniform(18.0);
+    let scale = rusttype::Scale::uniform(24.0);
     let v_metrics = font.v_metrics(scale);
     let mut y = padding as f32 + v_metrics.ascent;
 
     for line in lines {
+        println!("{}", line);
+        if line.is_empty() {
+            y += line_height as f32;
+            continue;
+        }
         let glyphs: Vec<_> = font.layout(&line, scale, point(0.0, y)).collect();
         let width = glyphs.iter().rev().next().unwrap().position().x as u32;
         let x = (MAX_WIDTH - width) / 2;
         for glyph in glyphs {
             if let Some(bb) = glyph.pixel_bounding_box() {
                 glyph.draw(|x, y, v| {
-                    let x = x as i32 + bb.min.x;
+                    let x = x as i32 + bb.min.x + CALENDAR_X_OFFSET;
                     let y = y as i32 + bb.min.y;
                     let coverage = 255 - ((v * 255.0) as u8);
-                    img.put_pixel(x as u32, y as u32, Rgb([coverage, coverage, coverage]));
+                    if x>=0 && y>=0 && x<MAX_WIDTH as i32 && y<MAX_HEIGHT as i32 {
+                        img.put_pixel(x as u32, y as u32, Rgb([coverage, coverage, coverage]));
+                    }
                 })
             }
         }
